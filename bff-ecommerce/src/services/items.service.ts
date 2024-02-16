a5ebfaa90b47a2cc6bd02c255a42cece8f67a67c
jonathangriguol <jonathan.griguol@gmail.com>
2024-02-16 02:22:21 -0300

    Limit search results to 4 items

--- bff-ecommerce/src/services/items.service.ts
+++ bff-ecommerce/src/services/items.service.ts
@@ -1,39 +1,42 @@
 import axios from "axios";
 import config from "../configs/config";
-import Item from "../interfaces/itemDetail.model";
 
 import { ITEMS_PATH, SITES_MLA_PATH } from "../constants/urls.constants";
 import { DECIMALS } from "../constants/bussiness.constants";
 
 const itemsService = {
-  getItems: async (searchQuery: string | undefined): Promise<Item[]> => {
+  getItems: async (searchQuery: string | undefined): Promise<any[]> => {
     try {
       const url = `${config.meli_api_url}/${SITES_MLA_PATH}/search?q=${
         searchQuery ? encodeURIComponent(searchQuery) : ""
       }`;
 
-      const response = await axios.get(url);
-      const items: Item[] = response.data.results.map((item: any) => {
-        return {
-          id: item.id,
-          title: item.title,
+      const { data } = await axios.get(url);
+
+      const items = [];
+  
+      for (let i = 0; i < Math.min(data?.results.length, 4); i++) {
+        items.push({
+          id: data.results[i].id,
+          title: data.results[i].title,
           price: {
-            currency: item.currency_id,
-            amount: item.price,
+            currency: data.results[i].currency_id,
+            amount: data.results[i].price,
             decimals: DECIMALS,
           },
-          picture: item.thumbnail,
-          condition: item.condition,
-          free_shipping: item.shipping.free_shipping,
-        };
-      });
+          picture: data.results[i].thumbnail,
+          condition: data.results[i].condition,
+          free_shipping: data.results[i].shipping.free_shipping,
+        });
+      }
 
       return items;
     } catch (error) {
       console.error(error);
-      throw new Error("Error al obtener los productos");
+      throw new Error("Error al obtener el listado de productos");
     }
   },
+
   getItemById: async (id) => {
     try {
       const url = `${config.meli_api_url}/${ITEMS_PATH}/${id}`;
@@ -43,7 +46,7 @@ const itemsService = {
       return response;
     } catch (error) {
       console.error(error);
-      throw new Error("Error al obtener los productos");
+      throw new Error("Error al obtener el producto");
     }
   },
 
@@ -56,7 +59,7 @@ const itemsService = {
       return response;
     } catch (error) {
       console.error(error);
-      throw new Error("Error al obtener los productos");
+      throw new Error("Error al obtener el detalle del producto");
     }
   },
 };
